d3734a113bbf44023ed52e7f3554baf22f32577b
razak17 <moabdulrazak17@gmail.com>
2022-01-27 15:38:01 +0000

    update border colors for dwm

--- .cache/wal/colors-wal-dwm.h
+++ .cache/wal/colors-wal-dwm.h
@@ -4,7 +4,7 @@ static const char norm_border[] = "#888989";
 
 static const char sel_fg[] = "#c3c5c4";
 static const char sel_bg[] = "#556B70";
-static const char sel_border[] = "#495458";
+static const char sel_border[] = "#556B70";
 
 static const char urg_fg[] = "#c3c5c4";
 static const char urg_bg[] = "#495458";
