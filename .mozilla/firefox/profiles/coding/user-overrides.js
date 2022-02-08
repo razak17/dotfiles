c26d5d53e6b1fd1b52c5892b4c7b0ed4215ae646
razak17 <moabdulrazak17@gmail.com>
2022-02-08 18:31:15 +0000

    upload: add user-overrides

---
+++ .mozilla/firefox/profiles/coding/user-overrides.js
@@ -0,0 +1,119 @@
+// HOMEPAGE
+user_pref("browser.newtabpage.activity-stream.feeds.section.highlights", false);
+user_pref("browser.newtabpage.activity-stream.feeds.topsites", false);
+
+// FONT
+user_pref("font.name.serif.x-western", "FreeSans");
+user_pref("font.size.variable.x-western", 12);
+user_pref("font.internaluseonly.changed", false);
+
+// SEARCH AND URL BAR
+user_pref("browser.urlbar.suggest.history", false);
+user_pref("browser.urlbar.suggest.bookmark", true);
+user_pref("browser.urlbar.suggest.openpage", true);
+user_pref("browser.urlbar.suggest.engines", false);
+user_pref("browser.urlbar.suggest.topsites", false);
+user_pref("browser.search.countryCode", "US");
+user_pref("browser.search.widget.inNavBar", true);
+user_pref("browser.search.geoSpecificDefaults", false);
+user_pref("browser.search.geoSpecificDefaults.url", "");
+user_pref("browser.urlbar.placeholderName", "DuckDuckGo");
+user_pref("browser.urlbar.placeholderName.private", "DuckDuckGo");
+
+// BOOKMARKS
+user_pref("browser.tabs.loadBookmarksInTabs", true); // open bookmarks in a new tab [FF57+]
+user_pref("browser.bookmarks.max_backups", 2);
+user_pref("browser.toolbars.bookmarks.showOtherBookmarks", false);
+user_pref("browser.toolbars.bookmarks.visibility", "never");
+
+// DEVTOOLS
+user_pref("devtools.theme", "dark");
+user_pref("devtools.editor.keymap", "vim");
+
+// WELCOME & WHAT's NEW NOTICES
+user_pref("startup.homepage_welcome_url", "");
+user_pref("startup.homepage_welcome_url.additional", "");
+user_pref("startup.homepage_override_url", ""); // What's New page after updates
+
+// WARNINGS
+user_pref("browser.tabs.warnOnClose", false);
+user_pref("browser.tabs.warnOnCloseOtherTabs", false);
+user_pref("browser.tabs.warnOnOpen", false);
+user_pref("full-screen-api.warning.delay", 0);
+user_pref("full-screen-api.warning.timeout", 0);
+
+// APPEARANCE
+user_pref("sidebar.position_start", false);
+// user_pref("browser.display.background_color", "#ff0000");
+user_pref("browser.display.background_color", "#1e2127");
+user_pref("browser.download.autohideButton", false); // [FF57+]
+user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true); // [FF68+] allow userChrome/userContent
+
+// CONTENT BEHAVIOR
+user_pref("accessibility.browsewithcaret", false);
+user_pref("accessibility.typeaheadfind", false);
+user_pref("clipboard.autocopy", false); // disable autocopy default [LINUX]
+user_pref("layout.spellcheckDefault", 2); // 0=none, 1-multi-line, 2=multi-line & single-line
+
+// UX BEHAVIOR
+user_pref("browser.backspace_action", 2); // 0=previous page, 1=scroll up, 2=do nothing
+user_pref("browser.quitShortcut.disabled", true); // disable Ctrl-Q quit shortcut [LINUX] [MAC] [FF87+]
+user_pref("browser.tabs.closeWindowWithLastTab", false);
+user_pref("browser.urlbar.decodeURLsOnCopy", true); // see bugzilla 1320061 [FF53+]
+user_pref("general.autoScroll", false); // middle-click enabling auto-scrolling [DEFAULT: false on Linux]
+user_pref("ui.key.menuAccessKey", 0); // disable alt key toggling the menu bar [RESTART]
+user_pref("view_source.tab", false); // view "page/selection source" in a new window [FF68+, FF59 and under]
+user_pref("full-screen-api.ignore-widgets", true);
+
+// UX FEATURES: disable and hide the icons and menus
+user_pref("extensions.pocket.enabled", false); // Pocket Account [FF46+]
+user_pref("extensions.screenshots.disabled", false); // [FF55+]
+user_pref("identity.fxaccounts.enabled", false); // Firefox Accounts & Sync [FF60+] [RESTART]
+user_pref("reader.parse-on-load.enabled", false); // Reader View
+
+/*** [SECTION 0400]: SAFE BROWSING (SB) ***/
+user_pref("browser.safebrowsing.malware.enabled", true);
+user_pref("browser.safebrowsing.phishing.enabled", true);
+user_pref("browser.safebrowsing.downloads.enabled", true);
+user_pref("layers.acceleration.disabled", false);
+
+/*** [SECTION 5000]: OPTIONAL OPSEC ***/
+user_pref("signon.rememberSignons", false);
+user_pref("browser.privatebrowsing.autostart", false);
+user_pref("extensions.formautofill.addresses.usage.hasEntry", false);
+
+/*** [SECTION 1600]: HEADERS / REFERERS ***/
+user_pref("privacy.userContext.newTabContainerOnLeftClick.enabled", true);
+
+/** EXTENSIONS ***/
+user_pref("extensions.webextensions.restrictedDomains", "");
+
+/*** [SECTION 0100]: STARTUP ***/
+user_pref("browser.startup.page", 3);
+
+/** SANITIZE ON SHUTDOWN : ALL OR NOTHING ***/
+user_pref("privacy.clearOnShutdown.cookies", false);
+user_pref("privacy.clearOnShutdown.history", false);
+user_pref("privacy.clearOnShutdown.openWindows", false);
+user_pref("privacy.cpd.openWindows", false);
+user_pref("privacy./cpd.history", false);
+user_pref("privacy.cpd.cookies", false);
+
+/*** [SECTION 2800]: SHUTDOWN & SANITIZING ***/
+user_pref("network.cookie.lifetimePolicy", 0);
+
+/** MIXED CONTENT ***/
+user_pref("dom.security.https_only_mode", false); // [FF76+]
+
+/*** [SECTION 0700]: DNS / DoH / PROXY / SOCKS / IPv6 ***/
+user_pref("network.dns.disableIPv6", false); // localhost:8000 not working
+
+/*** [SECTION 4500]: RFP (RESIST FINGERPRINTING) **/
+// user_pref("privacy.resistFingerprinting", false); // Cause of light theme bug
+// user_pref("privacy.resistFingerprinting.letterboxing", false); // [HIDDEN PREF]
+user_pref("key.url", "https://html.duckduckgo.com/html?q=\\");
+
+/*** [SECTION 9000]: PERSONAL
+   Non-project related but useful. If any interest you, add them to your overrides
+***/
+user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true); // [FF68+] allow userChrome/userContent
---
+++ .mozilla/firefox/profiles/user/user-overrides.js
@@ -0,0 +1,8 @@
+// HOMEPAGE
+user_pref("browser.newtabpage.activity-stream.feeds.section.highlights", false);
+user_pref("browser.newtabpage.activity-stream.feeds.topsites", false);
+
+/*** [SECTION 9000]: PERSONAL
+   Non-project related but useful. If any interest you, add them to your overrides
+***/
+user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true); // [FF68+] allow userChrome/userContent
