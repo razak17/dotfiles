04c029b3d204da48ac387acba598c96b97ca2130
razak17 <moabdulrazak17@gmail.com>
2022-02-21 12:53:16 +0000

    chore: update dwm

--- .mozilla/firefox/profiles/user/user-overrides.js
+++ .mozilla/firefox/profiles/user/user-overrides.js
@@ -1,6 +1,7 @@
 // HOMEPAGE
 user_pref("browser.newtabpage.activity-stream.feeds.section.highlights", false);
 user_pref("browser.newtabpage.activity-stream.feeds.topsites", false);
+user_pref("browser.newtabpage.activity-stream.section.highlights.includePocket", false);
 
 /*** [SECTION 9000]: PERSONAL
    Non-project related but useful. If any interest you, add them to your overrides
@@ -12,12 +13,26 @@ user_pref("network.trr.mode", 2);
 user_pref("network.trr.custom_uri", "https://doh.libredns.gr/dns-query");
 user_pref("network.trr.uri", "https://doh.libredns.gr/dns-query");
 
+// UX FEATURES: disable and hide the icons and menus
+user_pref("extensions.pocket.enabled", false); // Pocket Account [FF46+]
+user_pref("extensions.screenshots.disabled", false); // [FF55+]
+user_pref("identity.fxaccounts.enabled", false); // Firefox Accounts & Sync [FF60+] [RESTART]
+user_pref("reader.parse-on-load.enabled", false); // Reader View
+
+/*** [SECTION 0400]: SAFE BROWSING (SB) ***/
+user_pref("browser.safebrowsing.malware.enabled", true);
+user_pref("browser.safebrowsing.phishing.enabled", true);
+user_pref("browser.safebrowsing.downloads.enabled", true);
+user_pref("layers.acceleration.disabled", false);
+
+/*** [SECTION 5000]: OPTIONAL OPSEC ***/
+user_pref("signon.rememberSignons", false);
+user_pref("browser.privatebrowsing.autostart", false);
+user_pref("extensions.formautofill.addresses.usage.hasEntry", false);
+
 // Hardware acceleration
 user_pref("browser.preferences.defaultPerformanceSettings.enabled", false);
 user_pref("layers.acceleration.disabled", true);
-
-user_pref("browser.newtabpage.activity-stream.section.highlights.includePocket", false);
-user_pref("extensions.pocket.enabled", false); // Pocket Account [FF46+]
 
 user_pref("full-screen-api.warning.delay", 0);
 user_pref("full-screen-api.warning.timeout", 0);
