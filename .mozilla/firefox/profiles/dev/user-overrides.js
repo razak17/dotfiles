d2eee63efe6060ed5d211b488963861d34d7a83f
razak17 <moabdulrazak17@gmail.com>
2022-02-21 12:05:46 +0000

    refactor(firefox): add config for dev profile

---
+++ .mozilla/firefox/profiles/dev/user-overrides.js
@@ -0,0 +1,90 @@
+// HOMEPAGE
+user_pref("browser.newtabpage.activity-stream.feeds.section.highlights", false);
+user_pref("browser.newtabpage.activity-stream.feeds.topsites", false);
+
+/*** [SECTION 9000]: PERSONAL
+   Non-project related but useful. If any interest you, add them to your overrides
+***/
+user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true); // [FF68+] allow userChrome/userContent
+
+/*** [SECTION 0700]: DNS / DoH / PROXY / SOCKS / IPv6 ***/
+user_pref("network.trr.mode", 2);
+user_pref("network.trr.custom_uri", "https://doh.libredns.gr/dns-query");
+user_pref("network.trr.uri", "https://doh.libredns.gr/dns-query");
+
+// Hardware acceleration
+user_pref("browser.preferences.defaultPerformanceSettings.enabled", false);
+user_pref("layers.acceleration.disabled", true);
+
+user_pref("browser.newtabpage.activity-stream.section.highlights.includePocket", false);
+user_pref("extensions.pocket.enabled", false); // Pocket Account [FF46+]
+
+user_pref("full-screen-api.warning.delay", 0);
+user_pref("full-screen-api.warning.timeout", 0);
+user_pref("sidebar.position_start", false);
+
+/* 1212: set OCSP fetch failures (non-stapled, see 1211) to hard-fail [SETUP-WEB]
+ * When a CA cannot be reached to validate a cert, Firefox just continues the connection (=soft-fail)
+ * Setting this pref to true tells Firefox to instead terminate the connection (=hard-fail)
+ * It is pointless to soft-fail when an OCSP fetch fails: you cannot confirm a cert is still valid (it
+ * could have been revoked) and/or you could be under attack (e.g. malicious blocking of OCSP servers)
+ ***/
+user_pref("security.OCSP.require", false);
+
+/*** [SECTION 4500]: RFP (RESIST FINGERPRINTING) **/
+user_pref("privacy.resistFingerprinting", false); // Cause of light theme bug
+user_pref("privacy.resistFingerprinting.letterboxing", false); // reduced screen size
+
+// BOOKMARKS
+user_pref("browser.tabs.loadBookmarksInTabs", true); // open bookmarks in a new tab [FF57+]
+user_pref("browser.bookmarks.max_backups", 2);
+user_pref("browser.toolbars.bookmarks.showOtherBookmarks", false);
+user_pref("browser.toolbars.bookmarks.visibility", "never");
+
+// DEVTOOLS
+user_pref("devtools.theme", "dark");
+user_pref("devtools.editor.keymap", "vim");
+
+// WARNINGS
+user_pref("browser.tabs.warnOnClose", false);
+user_pref("browser.tabs.warnOnCloseOtherTabs", false);
+user_pref("browser.tabs.warnOnOpen", false);
+user_pref("full-screen-api.warning.delay", 0);
+user_pref("full-screen-api.warning.timeout", 0);
+
+// APPEARANCE
+user_pref("sidebar.position_start", false);
+user_pref("browser.download.autohideButton", false); // [FF57+]
+
+// UX FEATURES: disable and hide the icons and menus
+user_pref("extensions.pocket.enabled", false); // Pocket Account [FF46+]
+user_pref("extensions.screenshots.disabled", false); // [FF55+]
+user_pref("identity.fxaccounts.enabled", false); // Firefox Accounts & Sync [FF60+] [RESTART]
+user_pref("reader.parse-on-load.enabled", false); // Reader View
+
+/*** [SECTION 0400]: SAFE BROWSING (SB) ***/
+user_pref("browser.safebrowsing.malware.enabled", true);
+user_pref("browser.safebrowsing.phishing.enabled", true);
+user_pref("browser.safebrowsing.downloads.enabled", true);
+
+/** EXTENSIONS ***/
+user_pref("extensions.webextensions.restrictedDomains", "");
+
+/*** [SECTION 0700]: DNS / DoH / PROXY / SOCKS / IPv6 ***/
+user_pref("network.dns.disableIPv6", false); // localhost:8000 not working
+
+// Disable firefox suggest (Manually)
+user_pref("browser.urlbar.groupLabels.enabled", false)
+
+// SEARCH AND URL BAR
+user_pref("browser.urlbar.suggest.history", false);
+user_pref("browser.urlbar.suggest.bookmark", true);
+user_pref("browser.urlbar.suggest.openpage", true);
+user_pref("browser.urlbar.suggest.engines", false);
+user_pref("browser.urlbar.suggest.topsites", false);
+user_pref("browser.search.countryCode", "US");
+user_pref("browser.search.widget.inNavBar", true);
+user_pref("browser.search.geoSpecificDefaults", false);
+user_pref("browser.search.geoSpecificDefaults.url", "");
+user_pref("browser.urlbar.placeholderName", "DuckDuckGo");
+user_pref("browser.urlbar.placeholderName.private", "DuckDuckGo");
