0ea142da39766914d510657b32cc1aa10e2c960d
razak17 <moabdulrazak17@gmail.com>
2022-03-06 08:23:47 +0000

    chore(dwm,scripts,firefox): a few improvements

--- .mozilla/firefox/profiles/rec/user-overrides.js
+++ .mozilla/firefox/profiles/rec/user-overrides.js
@@ -1,7 +1,3 @@
-/*** [SECTION 4500]: RFP (RESIST FINGERPRINTING) **/
-user_pref("privacy.resistFingerprinting", false); // Cause of light theme bug
-user_pref("privacy.resistFingerprinting.letterboxing", false); // reduced screen size
-
 // HOMEPAGE
 user_pref("browser.newtabpage.activity-stream.feeds.section.highlights", false);
 user_pref("browser.newtabpage.activity-stream.feeds.topsites", false);
@@ -114,8 +110,8 @@ user_pref("key.url", "https://html.duckduckgo.com/html?q=\\");
 
 /*** [SECTION 0700]: DNS / DoH / PROXY / SOCKS / IPv6 ***/
 user_pref("network.trr.mode", 2);
-user_pref("network.trr.custom_uri", "https://doh.libredns.gr/dns-query");
-user_pref("network.trr.uri", "https://doh.libredns.gr/dns-query");
+user_pref("network.trr.custom_uri", "https://doh.libredns.gr/ads");
+user_pref("network.trr.uri", "https://doh.libredns.gr/ads");
 
 // Hardware acceleration
 user_pref("browser.preferences.defaultPerformanceSettings.enabled", false);
@@ -125,10 +121,18 @@ user_pref("layers.acceleration.disabled", true);
 user_pref("security.OCSP.require", false);
 
 // Disable firefox suggest (Manually)
-user_pref("browser.urlbar.groupLabels.enabled", false)
+user_pref("browser.urlbar.groupLabels.enabled", false) // manually set again after running updater.sh
 
 /* 1601: control when to send a cross-origin referer
  * 0=always (default), 1=only if base domains match, 2=only if hosts match
  * [SETUP-WEB] Breakage: older modems/routers and some sites e.g banks, vimeo, icloud, instagram
  * If "2" is too strict, then override to "0" and use Smart Referer extension (Strict mode + add exceptions) ***/
-user_pref("network.http.referer.XOriginPolicy", 0);
+user_pref("network.http.referer.XOriginPolicy", 0); // manually set again after running updater.sh or just comment out in user.js
+
+/* 2022: disable all DRM content (EME: Encryption Media Extension)
+ * Optionally hide the setting which also disables the DRM prompt
+ * [SETUP-WEB] e.g. Netflix, Amazon Prime, Hulu, HBO, Disney+, Showtime, Starz, DirectTV
+ * [SETTING] General>DRM Content>Play DRM-controlled content
+ * [TEST] https://bitmovin.com/demos/drm
+ * [1] https://www.eff.org/deeplinks/2017/10/drms-dead-canary-how-we-just-lost-web-what-we-learned-it-and-what-we-need-do-next ***/
+user_pref("media.eme.enabled", true);
