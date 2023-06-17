cab70382f972cd68cc753ad8b2720ac3d802191c
Razak Mo <moabdulrazak17@gmail.com>
2023-06-17 13:41:55 -0400

    chore(wal): add separate colors for dwm and dmenu
    - fix paru  command
    - update fnm path
    - update fzf install function

---
+++ .cache/wal/colors-wal-dmenu.h
@@ -0,0 +1,6 @@
+static const char *colors[SchemeLast][2] = {
+	/*     fg         bg       */
+	[SchemeNorm] = { "#bbbbbb", "#292e42" },
+	[SchemeSel] = { "#0e1616", "#9d7cd8" },
+	[SchemeOut] = { "#000000", "#00ffff" },
+};
---
+++ .cache/wal/colors-wal-dwm.h
@@ -0,0 +1,18 @@
+static const char norm_fg[] = "#c3c5c4";
+static const char norm_bg[] = "#292e42";
+static const char norm_border[] = "#1e2127";
+
+static const char sel_fg[] = "#0e1616";
+static const char sel_bg[] = "#9d7cd8";
+static const char sel_border[] = "#9d7cd8";
+
+static const char urg_fg[] = "#c3c5c4";
+static const char urg_bg[] = "#ff0000";
+static const char urg_border[] = "#ffff00";
+
+static const char *colors[][3]      = {
+    /*               fg           bg         border                         */
+    [SchemeNorm] = { norm_fg,     norm_bg,   norm_border }, // unfocused wins
+    [SchemeSel]  = { sel_fg,      sel_bg,    sel_border },  // the focused win
+};
+
