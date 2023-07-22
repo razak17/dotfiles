636dbef9b27963b5a6d1b2e0c2e92852b00d5302
Razak Mo <moabdulrazak17@gmail.com>
2023-07-22 16:29:10 +0000

    chore(wal): use updated colors
    - add deno install command
    - add sync dir to zshenv
    - use updated colors for dmenu

--- .cache/wal/colors-wal-dmenu.h
+++ .cache/wal/colors-wal-dmenu.h
@@ -1,6 +1,6 @@
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
 	[SchemeNorm] = { "#bbbbbb", "#292e42" },
-	[SchemeSel] = { "#0e1616", "#9d7cd8" },
+	[SchemeSel] = { "#0e1616", "#1d7c78" },
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
