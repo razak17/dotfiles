3d5e3a79a294595fb5c3fa9fc4c3a7fd408f5272
Razak Mo <moabdulrazak17@gmail.com>
2023-10-24 19:56:19 +0000

    chore(bin): add scripts

---
+++ .config/wezterm/wezterm.lua
@@ -0,0 +1,11 @@
+local wezterm = require("wezterm")
+
+return {
+	font = wezterm.font("JetBrains Mono Nerd Font"),
+	font_size = 10,
+	cursor_blink_rate = 800,
+	-- cursor_thickness = "2px",
+	-- line_height = "2.5px",
+	-- underline_position = "1px",
+	-- underline_thickness = "2px",
+}
