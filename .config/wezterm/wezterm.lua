2edcbe358c92f11772d4156d62365baa25a5f0b4
Razak Mo <moabdulrazak17@gmail.com>
2024-05-27 09:03:29 +0000

    chore(wezterm): disable tab bar, add alias

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,9 +1,19 @@
 local wezterm = require("wezterm")
 
 return {
-	font = wezterm.font("JetBrains Mono Nerd Font"),
+	-- font = wezterm.font("JetBrains Mono Nerd Font"),
+  font = wezterm.font('JetBrains Mono'),
 	font_size = 10,
 	cursor_blink_rate = 800,
+  enable_tab_bar = false,
+  window_background_opacity = 1.0,
+  window_decorations = 'RESIZE',
+  window_padding = {
+    left = 0,
+    right = 0,
+    top = 0,
+    bottom = 0,
+  }
 	-- cursor_thickness = "2px",
 	-- line_height = "2.5px",
 	-- underline_position = "1px",
