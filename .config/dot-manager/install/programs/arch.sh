42182bf19e8acdf9c09783977caf4ab46e09a084
Abdul Razak <moabdulrazak17@gmail.com>
2026-08-07 17:09:45 +0000

    feat(dot-manager): some packages need the extra repo

--- .config/dot-manager/install/programs/arch.sh
+++ .config/dot-manager/install/programs/arch.sh
@@ -9,18 +9,49 @@ install_arch_essentials() {
     xorg-server xorg-xinit xorg-xkill xorg-xsetroot xorg-xbacklight \
     xorg-xprop xorg-xinput xorg-xdpyinfo arc-gtk-theme grub-customizer \
     wireless_tools imagemagick bluez bluez-utils blueman openvpn btop \
-    xf86-input-libinput android-tools android-file-transfer ninja glow \
+    xf86-input-libinput android-tools android-file-transfer ninja \
     zathura zathura-ps zathura-pdf-poppler zathura-djvu zathura-cb \
-    brightnessctl sxiv nsxiv maim hwinfo ueberzug lolcat tokei eza \
+    brightnessctl sxiv nsxiv maim hwinfo ueberzug lolcat eza \
     ttf-times-new-roman pandoc aspell aspell-en dunst xclip hyperfine \
     mediainfo mpv mpd mpc pipewire pipewire-pulse wireplumber picom \
-    aria2 cowsay ranger screenkey ncmpcpp redshift zoxide cmatrix \
-    thunderbird syncthing lazygit git-delta slock gparted baobab \
-    bottom plocate rsync reflector gvfs gvfs-mtp zsync ntfs-3g \
-    direnv ctags libnotify strace mtools expac dosfstools elinks \
-    entr freeze global libconfig pngquant pamixer
+    aria2 cowsay ranger ncmpcpp redshift zoxide cmatrix \
+    thunderbird syncthing lazygit slock gparted baobab \
+    plocate rsync reflector gvfs gvfs-mtp ntfs-3g \
+    ctags libnotify strace mtools expac dosfstools elinks \
+    entr libconfig pamixer
 
   log "success" "Arch essentials installed."
 }
 
-install_arch_essentials "$@"
+install_arch_extras() {
+  # These packages are in the extras repo, but not installed by default
+
+  print_step "Installing Arch extras"
+
+  __install_package_arch artix-archlinux-support
+
+  # You need to enable the artix-archlinux-support repo in /etc/pacman.conf to install these packages
+  # see: https://wiki.artixlinux.org/Main/Repositories
+
+  __install_package_arch glow tokei screenkey git-delta bottom zsync direnv freeze global pngquant
+
+  log "success" "Arch extras installed."
+
+}
+
+do_program_install() {
+  case "$1" in
+  essentials) install_arch_essentials ;;
+  extras) install_arch_extras ;;
+  *)
+    log "error" "Unknown action: $1"
+    return 1
+    ;;
+  esac
+}
+
+if [ $# -eq 0 ]; then
+  install_arch_essentials "$@"
+else
+  do_program_install "$@"
+fi
